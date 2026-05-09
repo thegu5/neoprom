d26c645b49a39c24accb4261d3ce4c16886c8791
Gus <58223632+thegu5@users.noreply.github.com>
2026-05-09 01:52:02 -0400

    feat: add bucket generators

---
+++ src/bucketGenerators.ts
@@ -0,0 +1,34 @@
+// using node:assert/strict would make sense
+// TODO: check runtime support
+
+// TODO: double-check "Count MUST include the +Inf bucket"
+// https://prometheus.io/docs/instrumenting/writing_clientlibs/#histogram
+
+export function linearBuckets(start: number, width: number, count: number) {
+	// assert(count >= 1, "Linear buckets needs a positive count");
+	if (count < 1) {
+		throw new Error("Linear buckets needs a positive count");
+	}
+
+	return Array.from({ length: count }, (_, i) => start + i * width);
+}
+
+export function exponentialBuckets(
+	start: number,
+	factor: number,
+	count: number,
+) {
+	// assert(start > 0, "Exponential buckets needs a positive start");
+	// assert(count >= 1, "Exponential buckets needs a positive count");
+	// assert(factor > 1, "Exponential buckets needs a factor greater than 1");
+	if (start <= 0) {
+		throw new Error("Exponential buckets needs a positive start");
+	}
+	if (count < 1) {
+		throw new Error("Exponential buckets needs a positive count");
+	}
+	if (factor <= 1) {
+		throw new Error("Exponential buckets needs a factor greater than 1");
+	}
+	return Array.from({ length: count }, (_, i) => start * factor ** i);
+}
