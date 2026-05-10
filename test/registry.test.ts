5306b07aa809f7725c70a526e847b7f546d03d53
Gus <58223632+thegu5@users.noreply.github.com>
2026-05-10 12:15:11 -0400

    test: comment out temp registry "tests"

--- test/registry.test.ts
+++ test/registry.test.ts
@@ -1,35 +1,35 @@
-import { beforeEach, describe, it } from "node:test";
-import { Counter } from "../src/counter.ts";
-import { Histogram } from "../src/histogram.ts";
-import { globalRegistry } from "../src/registry.ts";
+// import { beforeEach, describe, it } from "node:test";
+// import { Counter } from "../src/counter.ts";
+// import { Histogram } from "../src/histogram.ts";
+// import { globalRegistry } from "../src/registry.ts";
 
-// none of these are actual tests yet, need to do assertions
-describe("Registry", () => {
-	beforeEach(() => {
-		globalRegistry.clear();
-	});
-	it("works? (temporary test)", async () => {
-		const testCounter = new Counter({
-			name: `test
-            Counter`,
-			help: "yup",
-			labelNames: ["f oo"],
-		});
-		testCounter.inc();
-		testCounter.inc({ "f oo": "bar" }, 2);
-		console.log(await globalRegistry.getMetrics());
-	});
-	it("does histogram stuff", async () => {
-		const testHistogram = new Histogram({
-			name: "a_histogram",
-			help: ":)",
-			labelNames: ["foo", "bar"],
-		});
-		testHistogram.observe({ foo: "asdf" }, 0.01);
-		testHistogram.observe(10);
-		testHistogram.observe({ foo: "asdf", bar: "awawa" }, 5);
-		testHistogram.observe(0.1);
-		testHistogram.observe(0.5);
-		console.log(await globalRegistry.getMetrics());
-	});
-});
+// // none of these are actual tests yet, need to do assertions
+// describe("Registry", () => {
+// 	beforeEach(() => {
+// 		globalRegistry.clear();
+// 	});
+// 	it("works? (temporary test)", async () => {
+// 		const testCounter = new Counter({
+// 			name: `test
+//             Counter`,
+// 			help: "yup",
+// 			labelNames: ["f oo"],
+// 		});
+// 		testCounter.inc();
+// 		testCounter.inc({ "f oo": "bar" }, 2);
+// 		console.log(await globalRegistry.getMetrics());
+// 	});
+// 	it("does histogram stuff", async () => {
+// 		const testHistogram = new Histogram({
+// 			name: "a_histogram",
+// 			help: ":)",
+// 			labelNames: ["foo", "bar"],
+// 		});
+// 		testHistogram.observe({ foo: "asdf" }, 0.01);
+// 		testHistogram.observe(10);
+// 		testHistogram.observe({ foo: "asdf", bar: "awawa" }, 5);
+// 		testHistogram.observe(0.1);
+// 		testHistogram.observe(0.5);
+// 		console.log(await globalRegistry.getMetrics());
+// 	});
+// });
